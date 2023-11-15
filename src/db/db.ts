220f69f28149fa7b88d4d75e3641fefee0a0d2ee
PorcoRosso85 <1.is.universe@gmail.com>
2023-11-15 09:40:39 +0900

    init

---
+++ src/db/db.ts
@@ -0,0 +1,15 @@
+import {
+  drizzle,
+  type BetterSQLite3Database,
+} from "drizzle-orm/better-sqlite3";
+import Database from "better-sqlite3";
+
+import * as schema from "./schema";
+
+const sqlite = new Database("sqlite3.db");
+const db: BetterSQLite3Database<typeof schema> = drizzle(sqlite, { schema });
+
+await db.insert(schema.users).values({ name: "Por" });
+
+const result = await db.select().from(schema.users);
+console.log("select result: ", result);
